afb22e44fc0bdf5f8972d5c6467bd9a9c33b7493
mataniss <matanissler@gmail.com>
2024-05-12 21:50:57 +0300

    added icon and action to the login button

--- client/app/src/main/java/com/matan/digitalstore/Login.java
+++ client/app/src/main/java/com/matan/digitalstore/Login.java
@@ -1,6 +1,9 @@
 package com.matan.digitalstore;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class Login extends AppCompatActivity {
+    private EditText usernameField;
+    private EditText passwordField;
+    private Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +25,17 @@ public class Login extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        usernameField = findViewById(R.id.editTextUsername);
+        passwordField = findViewById(R.id.editTextPassword);
+        loginButton = findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = String.valueOf(usernameField.getText());
+                String password = String.valueOf(passwordField.getText());
+                System.out.println("!!!");
+            }
         });
     }
 }
