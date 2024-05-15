4294b44bb0213a403d364438165c69f1a56be401
mataniss <matanissler@gmail.com>
2024-05-15 22:40:25 +0300

    added action to the sign up button

--- client/app/src/main/java/com/matan/digitalstore/SignUpActivity.java
+++ client/app/src/main/java/com/matan/digitalstore/SignUpActivity.java
@@ -1,14 +1,25 @@
 package com.matan.digitalstore;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.matan.digitalstore.Utils.HttpUtil;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.ResponseBody;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -31,8 +42,50 @@ public class SignUpActivity extends AppCompatActivity {
         submitButton = findViewById(R.id.submit);
 
         submitButton.setOnClickListener(view -> {
-            // Handle the click event for the submit button.
-            // You can write your own code here to handle the sign up logic.
+            if(String.valueOf(passwordEditText.getText())
+                    .equals(String.valueOf(confirmPasswordEditText.getText()))){
+                SignUpAction signUpAction = new SignUpAction();
+                signUpAction.execute();
+            }
+            else {
+                Toast.makeText(getApplicationContext(),"Passwords don't match",Toast.LENGTH_LONG).show();
+            }
+
         });
+    }
+
+    public class SignUpAction extends AsyncTask< Integer, Integer, Long> {
+
+        @Override
+        protected Long doInBackground(Integer...integers) {
+            Long userID = null;
+            String username = String.valueOf(usernameEditText.getText());
+            String password = String.valueOf(passwordEditText.getText());
+            String email = String.valueOf(emailEditText.getText());
+            try {
+                JSONObject json = new JSONObject();
+                json.put("username",username);
+                json.put("password", password);
+                json.put("email", email);
+                ResponseBody responseBody = HttpUtil.postRequest("users/signup",json,false);
+                userID = Long.valueOf(responseBody.string());
+
+            } catch (IOException | JSONException e) {
+                System.err.println("An error occurred during signup.");
+            }
+            return userID;
+        }
+
+        @Override
+        protected void onPostExecute(Long purchaseID) {
+            if(purchaseID == null){
+                String message = "An error occurred during signup.";
+                Toast.makeText(getApplicationContext(),message,Toast.LENGTH_LONG).show();
+            }
+            else {
+                Toast.makeText(getApplicationContext(),"Sign up was successful.",Toast.LENGTH_LONG).show();
+                finish();
+            }
+        }
     }
 }
